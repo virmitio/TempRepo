0c5a39cd5ec9b55453a195520704ab456f6b9525
Tim Rogers <tiroger@microsoft.com>
2013-01-23 15:24:22 -0800

    still in progress

--- BaseCmdlets/RegistryComparison.cs
+++ BaseCmdlets/RegistryComparison.cs
@@ -19,7 +19,6 @@ namespace VMProvisioningAgent
                 file = FileName;
             }
             
-
             public bool Init()
             {
                 if (file == null || file.Equals(String.Empty))
@@ -34,7 +33,7 @@ namespace VMProvisioningAgent
             }
         }
 
-        private Hive hiveA, hiveB;
+        private readonly Hive hiveA, hiveB;
         public string HiveA { get { return hiveA.file; } }
         public string HiveB { get { return hiveB.file; } }
 
@@ -43,20 +42,91 @@ namespace VMProvisioningAgent
             hiveA = new Hive(HiveFileA);
             hiveB = new Hive(HiveFileB);
         }
+        
+        public class Data
+        {
+            public RegistryValueKind TypeA { get; private set; }
+            public object ValueA { get; private set; }
+            public RegistryValueKind TypeB { get; private set; }
+            public object ValueB { get; private set; }
 
 
-        // output storage container
-        // DoCompare()
+            public Data(){}
 
+            public Data(object aVal, RegistryValueKind aType, object bVal, RegistryValueKind bType)
+            {
+                TypeA = aType;
+                TypeB = bType;
+                ValueA = aVal;
+                ValueB = bVal;
+            }
+
+            public void SetA(object aVal, RegistryValueKind aType)
+            {
+                TypeA = aType;
+                ValueA = aVal;
+            }
+
+            public void SetB(object bVal, RegistryValueKind bType)
+            {
+                TypeA = bType;
+                ValueA = bVal;
+            }
+        }
+
+        private Dictionary<string, Data> _output = new Dictionary<string, Data>();
+        public Dictionary<string, Data> Output { get{return new Dictionary<string, Data>(_output);} }
 
         public bool DoCompare()
         {
-            return hiveA.Init() && hiveB.Init() ? InnerCompare(hiveA.root, hiveB.root, ref Output) : false;
+            return hiveA.Init() && hiveB.Init() && InnerCompare(hiveA.root, hiveB.root, @"\", ref _output);
         }
 
-        private static bool InnerCompare(RegistryKey A, RegistryKey B, ref ?? output)
+        private static bool InnerCompare(RegistryKey A, RegistryKey B, string root,ref Dictionary<string, Data> output)
         {
-            
+            bool current = true;
+            try
+            {
+                if (A != null)
+                {
+                    // Process A
+                    foreach (var Name in A.GetValueNames())
+                    {
+                        string EntryName = root + A.Name + "::" + Name;
+                        var dat = new Data();
+                        dat.SetA(A.GetValue(Name), A.GetValueKind(Name));
+                        output.Add(EntryName, dat);
+                    }
+                    foreach (var keyName in A.GetSubKeyNames())
+                    {
+                        RegistryKey subA = A.OpenSubKey(keyName);
+                        RegistryKey subB = B == null ? null : B.OpenSubKey(keyName);
+                        current &= InnerCompare(subA, subB, root + keyName + @"\", ref output);
+                    }
+                }
+                if (B != null)
+                {
+                    foreach (var Name in B.GetValueNames())
+                    {
+                        string EntryName = root + B.Name + "::" + Name;
+                        Data dat = output.ContainsKey(EntryName) ? output[EntryName] : new Data();
+                        dat.SetB(B.GetValue(Name), B.GetValueKind(Name));
+                        output[EntryName] = dat;
+                    }
+                    foreach (var keyName in B.GetSubKeyNames())
+                    {
+                        // when we get here, we have already cleared everything present in the A side
+                        RegistryKey subB = B.OpenSubKey(keyName);
+                        current &= InnerCompare(null, subB, root + keyName + @"\", ref output);
+                    }
+
+                }
+                return current;
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
         }
 
     }
